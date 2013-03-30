5d5b91a94b7fdeebe52fd19ea157a2b6269a7382
bestofwon <ddalpang22@naver.com>
2013-03-30 16:41:16 +0900

    fb_login setting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130329110942) do
+ActiveRecord::Schema.define(:version => 20130330072044) do
 
   create_table "todos", :force => true do |t|
     t.string   "title"
@@ -36,6 +36,10 @@ ActiveRecord::Schema.define(:version => 20130329110942) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "token"
+    t.datetime "auth_expires_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
