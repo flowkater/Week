69fa48b893b4d3c5e7221ce044a4f2370e0841e0
everytime1004 <ha621209@naver.com>
2013-03-30 14:30:56 +0900

    add_columns

--- db/migrate/20130329110927_create_weekplans.rb
+++ db/migrate/20130329110927_create_weekplans.rb
@@ -2,7 +2,14 @@ class CreateWeekplans < ActiveRecord::Migration
   def change
     create_table :weekplans do |t|
 
+    	# 3-29.md
     	t.references :user
+
+  		# 3-30.md - add columns
+    	t.string :title
+    	t.integer :progress_rate
+    	t.boolean :public_friends
+    	t.boolean :public_all
 
       t.timestamps
     end
