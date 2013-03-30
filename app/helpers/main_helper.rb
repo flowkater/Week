027313316048e709ba18fc057e484e14db5b0289
everytime1004 <ha621209@naver.com>
2013-03-30 21:01:48 +0900

    요번주 계획 가져오기 하는 중

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,9 +1,14 @@
 module MainHelper
 	def ThisWeekPeople
 		# Time.gm(time.year, time.month, time.day) - Time.now.wday * 86400, time = Time.now
+		# Weekplan.first.created_at.day - Weekplan.first.created_at.wday
 		time = Time.now
-		thisWeek = Time.gm(time.year, time.month, time.day) - Time.now.wday * 86400
-		Weekplan.
+		thisWeekSunday = (Time.gm(time.year, time.month, time.day) - Time.now.wday * 86400)
+		thisWeekSaturday = thisWeekSunday + 7.days + 86399
+
+
+
+		
 
 	end
 end
