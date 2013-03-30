17c0fef20d16c12174bf183f4502dafe2dda5fae
everytime1004 <ha621209@naver.com>
2013-03-30 20:04:11 +0900

    weekplan 추가 수정

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,9 @@
 module MainHelper
+	def ThisWeekPeople
+		# Time.gm(time.year, time.month, time.day) - Time.now.wday * 86400, time = Time.now
+		time = Time.now
+		thisWeek = Time.gm(time.year, time.month, time.day) - Time.now.wday * 86400
+		Weekplan.
+
+	end
 end
