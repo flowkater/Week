43294791496894907bf9951c9a04e47c169f370d
everytime1004 <ha621209@naver.com>
2013-03-31 18:37:02 +0900

    이번 주 사람들 불러오기

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,6 @@ class MainController < ApplicationController
 		thisWeekSunday = (Time.gm(time.year, time.month, time.day) - Time.now.wday * 86400)
 		thisWeekSaturday = thisWeekSunday + 6.days + 86399
 
-		@thisWeekPeople = Weekplan.find(:all, :conditions => ["created_at >= ? AND created_at <= ?", thisWeekSunday, thisWeekSaturday])
-		@weekplan = Weekplan.all
+		@thisWeekPeople = Weekplan.find(:all, :conditions => ["created_at >= ? AND created_at <= ?", thisWeekSunday, thisWeekSaturday], :order => 'created_at DESC')
   end
 end
